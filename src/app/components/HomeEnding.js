a4a05abf31c19012d98bcceadecfa1ec3c292636
Lindsey Pak <lindseypak@mac.mynetworksettings.com>
2025-04-08 08:26:48 -0400

    last home section

---
+++ src/app/components/HomeEnding.js
@@ -0,0 +1,14 @@
+import "../styles/home.scss";
+
+const HomeEnding = () => {
+  return (
+    <div className="homeEnding">
+      <div className="homeEnding__text">
+        <h6>let's go and </h6>
+        <h6>explore</h6>
+      </div>
+    </div>
+  );
+};
+
+export default HomeEnding;
--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@
 import "./styles/home.scss";
 import Hero from "./components/Hero";
 import ILike from "./components/ILike";
+import HomeEnding from "./components/HomeEnding";
 
 export default function Home() {
   return (
     <div className="home">
       <Hero />
       <ILike />
+      <HomeEnding />
     </div>
   );
 }
